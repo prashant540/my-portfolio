73b7750c591b5a0ec2f259de6b7a1f5ecc1a8302
Prashant <prashant@Prashants-MacBook-Air.local>
2018-05-03 21:27:08 +0530

    added new and index page for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,20 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolios = Portfolio.all
 	end
+
+	def new
+		@portfolio_item = Portfolio.new
+	end
+
+	def create
+    @portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle,:body))
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
