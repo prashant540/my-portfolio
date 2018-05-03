f2fd78706d162cc2b49beb9250b5de836840b108
Prashant <prashant@Prashants-MacBook-Air.local>
2018-05-03 22:22:57 +0530

    added edit and udpate

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,14 +9,29 @@ class PortfoliosController < ApplicationController
 	end
 
 	def create
-    @portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle,:body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle,:body,:thumb_image,:main_image))
     respond_to do |format|
-      if @portfolio.save
+      if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully created.' }
       else
         format.html { render :new }
       end
     end
+  end
+
+  def edit
+  	@portfolio_item = Portfolio.find_by_id(params[:id])
+  end
+
+  def update
+  	@portfolio_item = Portfolio.find_by_id(params[:id])
+  	respond_to do |format|
+	  	if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body, :thumb_image, :main_image))
+	      format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
+	    else
+	      format.html { render :new }
+	    end
+	  end
   end
 
 end
