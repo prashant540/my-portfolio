ebe6444635de1a49971cbed97e915dcc47d529f6
Prashant <prashant@Prashants-MacBook-Air.local>
2018-05-05 23:03:08 +0530

    added delete link

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -12,6 +12,15 @@ class PortfoliosController < ApplicationController
 		@portfolio_item = Portfolio.find(params[:id])
 	end
 
+	def destroy
+		@portfolio_item = Portfolio.find(params[:id])
+		@portfolio_item.destroy	
+		respond_to do |format|
+    	format.html { redirect_to portfolios_url, notice: 'Portfolio item was deleted successfully' }
+    end
+	end
+
+
 	def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle,:body,:thumb_image,:main_image))
     respond_to do |format|
