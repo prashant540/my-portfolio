7b1a5e0bb35975d37dd173fcaeee66f7b4572242
Prashant <prashant@Prashants-MacBook-Air.local>
2018-05-06 10:15:45 +0530

    added custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/contact'
-
+  resources :portfolios, except: [:show]
+  get 'portfolios/:id' ,to: 'portfolios#show', as: 'portfolio_show'
+  get 'about' ,to: 'pages#about'
+  get 'contact' , to: 'pages#contact'
+  root to: "pages#home"
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
