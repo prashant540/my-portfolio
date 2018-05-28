b3d58007cdccffe093d8f50d86124c5b677a680c
Prashant <prashant@Prashants-MacBook-Air.local>
2018-05-28 21:51:56 +0530

    added a concern to manage with data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+	# include PlaceHolder
 	validates_presence_of :title,:body,:main_image,:thumb_image
 
 	scope :ruby_on_rails_items, -> {where(subtitle: 'Ruby on Rails')}
@@ -8,7 +9,7 @@ class Portfolio < ApplicationRecord
 
 
 	def set_defaults
-		self.main_image ||= "http://via.placeholder.com/600x400"
-		self.thumb_image ||= "http://via.placeholder.com/350x200"
+		self.main_image ||= PlaceHolder.image_generator(600,400)
+		self.thumb_image ||= PlaceHolder.image_generator(350,200)
 	end
 end
